d5df4d711ecac032dbad07c48ed810a21d537ed3
Fabricio M <fabricio2111@gmail.com>
2020-10-22 00:03:35 -0500

    Add requirements

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,8 @@
 #!/bin/bash/
-# Script to install required dependencies, run bash ./setup.sh
+# Script to install required dependencies
 conda install --yes pandas
 conda install --yes numpy
 conda install --yes scikit-learn
+conda install --yes seaborn
+pip install --user -U vaderSentiment
+pip install --user -U nltk
